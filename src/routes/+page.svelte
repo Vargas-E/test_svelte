4a0b53b30e93f858bee5276ffa61a913e61e9470
Vargas-E <vargasivanezequiel@gmail.com>
2023-08-27 23:47:44 -0300

    new try

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<button on:click={() => console.log("asdasdasd")}>asdasdsda</button>
+</section>
+
